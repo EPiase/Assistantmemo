b2eaa1bb952a21ec02e843ee19b839df97fbca4f
alexciccolella125 <alexciccolella125@gmail.com>
2021-11-10 23:58:44 -0500

    upload audio, update firestore, & speech-to-text complete

--- API/app/main.py
+++ API/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi import responses
 from fastapi.responses import HTMLResponse
 from fastapi import UploadFile, File, Form
+from fastapi.responses import FileResponse
 # from google.api_core import retry
 from routers import notes
 
@@ -15,76 +17,120 @@ async def root():
     return res.read()
     # return var
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+@app.post("/create-note/")
+async def create_note(user_id: str, file: UploadFile = File(...)):
     from google.cloud import storage
-    # import tempfile
-    import aiofiles
+    from google.cloud import firestore
+    from google.cloud import speech
+    import tempfile
     import uuid
-    # tempfile.NamedTemporaryFile
-    filename = str(uuid.uuid4())
+    import datetime
+
+    # create file names
+    audio_filename = str(uuid.uuid4()) + file.filename
+    note_filename = str(uuid.uuid4())
+    
+    # access GCS
     storage_client = storage.Client()
     bucket = storage_client.bucket("assistantmemo-recordings")
-    blob = bucket.blob(f"{filename}{file.filename}")
+    blob = bucket.blob(audio_filename)
     
-    async with aiofiles.open(f"{filename}{file.filename}", 'wb') as out_file:
-        content = await file.read()
-        await out_file.write(content)
-        print(type(out_file))
-        blob.upload_from_filename(out_file.name)
+    # upload incoming audio file
+    with tempfile.NamedTemporaryFile() as named_temp_file:
+        named_temp_file.write(await file.read())
+        blob.upload_from_filename(named_temp_file.name)
+        # return {"audio_filename": audio_filename, "local_temp_name": named_temp_file.name}
 
-    # print(
-    #     "File {} uploaded to {}.".format(
-    #         file.filename, destination_blob_name
-    #     )
-    # )
-    return {"filename": out_file.name, "destination_blob_name": file.filename}
+    # access firestore
+    db = firestore.Client()
+    # access and set note in database
+    doc_ref = db.collection('users').document(user_id).collection('notes').document(note_filename)
+    doc_ref.set({
+        'audio_filename': audio_filename,
+        'classification': 'misc',
+        'date_recorded': datetime.datetime.utcnow(),
+        'is_starred': False,
+        'text_transcript': ''
+    })
 
-
-
-@app.post("/file/")
-async def transcribe_file(speech_file: UploadFile = File(...)):
-    """Transcribe the given audio file asynchronously."""
-    from google.cloud import speech
-
+    # access speech to text api
     client = speech.SpeechClient()
-
-    """
-     Note that transcription is limited to a 60 seconds audio file.
-     Use a GCS file for audio longer than 1 minute.
-    """
-    audio = speech.RecognitionAudio(content=await speech_file.read())
-
+    gcs_uri = f'gs://assistantmemo-recordings/{audio_filename}'
+    audio = speech.RecognitionAudio(uri=gcs_uri)
     config = speech.RecognitionConfig(language_code="en-US")
-
-
     operation = client.long_running_recognize(config=config, audio=audio)
-
-    print("Waiting for operation to complete...")
     response = operation.result(timeout=90)
-    print(response)
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
     Transcript = []
-
     for result in response.results:
         # The first alternative is the most likely one for this portion.
         Transcript.append(result.alternatives[0].transcript)
-
     return Transcript
 
-# filename = "84-121550-0006.flac"
 
-# @app.post("/speech-text")
-# async def speech_text():
-#     import speech_recognition as sr
-#     # initialize the recognizer
-#     r = sr.Recognizer()
-#     # open the file
-#     with sr.AudioFile(filename) as source:
-#         # listen for the data (load audio to memory)
-#         audio_data = r.record(source)
-#         # recognize (convert from speech to text)
-#         text = r.recognize_google(audio_data)
-#         print(text)
-#         return text
+
+
+
+
+@app.get("/download_file")
+async def download_file(bucket: str, sblob: str):
+    from google.cloud import storage
+    from starlette.responses import StreamingResponse
+    import io
+    import tempfile
+
+    storage_client = storage.Client()
+    # get bucket with name
+    bucket = storage_client.get_bucket(bucket)
+    # get bucket data as blob
+    blob = bucket.get_blob(sblob)
+
+    return StreamingResponse(io.BytesIO(blob.download_as_bytes()), media_type='audio/flac')
+
+# @app.get("/firebase")
+# async def get_firebase_users():
+#     from google.cloud import firestore
+#     db = firestore.Client()
+
+#     # Add a new document
+#     # doc_ref = db.collection('users').document('YvVBPoGPal8VVQmCdnnd').collection('notes').document('FWsAw2vjsEl3R1HJgwy0')
+#     # doc_ref.set({
+#     #     'audio_url': '',
+#     #     'classification': '/Computers & Electronics/Computer Hardware/Computer Components',
+#     #     'date_recorded': '11/10/21, 7:44 PM',
+#     #     'is_starred': False,
+#     #     'text_transcript': 'I will be getting a new intel processor over the coming weeks.'
+#     # })
+
+#     # Then query for documents
+#     users_ref = db.collection('users').document('YvVBPoGPal8VVQmCdnnd').collection('notes')
+#     return ['{} => {}'.format(doc.id, doc.to_dict()) for doc in users_ref.stream()]
+
+# async def transcribe_file(speech_file: UploadFile = File(...)):
+#     """Transcribe the given audio file asynchronously."""
+#     from google.cloud import speech
+
+#     client = speech.SpeechClient()
+
+#     """
+#      Note that transcription is limited to a 60 seconds audio file.
+#      Use a GCS file for audio longer than 1 minute.
+#     """
+#     audio = speech.RecognitionAudio(content=await speech_file.read())
+
+#     config = speech.RecognitionConfig(language_code="en-US")
+
+
+#     operation = client.long_running_recognize(config=config, audio=audio)
+
+#     print("Waiting for operation to complete...")
+#     response = operation.result(timeout=90)
+#     print(response)
+#     # Each result is for a consecutive portion of the audio. Iterate through
+#     # them to get the transcripts for the entire audio file.
+#     Transcript = []
+
+#     for result in response.results:
+#         # The first alternative is the most likely one for this portion.
+#         Transcript.append(result.alternatives[0].transcript)
+
+#     return Transcript
