f207df97599c92f3de4aad0eddbc0999f6998338
AlexCiccolella125 <alessandro@blusleepproducts.com>
2021-11-30 10:34:25 -0500

    quicksave

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,16 +1,48 @@
-import 'package:assistantmemo/homepage.dart';
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
+// import pages
+import 'package:assistantmemo/homepage.dart';
+
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text('error');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            home: HomePage(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text('loading', textDirection: TextDirection.ltr);
+      },
     );
   }
 }
