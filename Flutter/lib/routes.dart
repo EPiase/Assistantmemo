c28da9efd66a743c7889bacadadeca009e5831ef
Eric Piasentin <piase@Erics-MacBook-Pro.local>
2021-12-08 09:41:03 -0500

    One page half work

--- Flutter/lib/routes.dart
+++ Flutter/lib/routes.dart
@@ -1,15 +1,19 @@
 // import 'dart:js';
 
+// import 'dart:js';
+
 import 'package:assistantmemo/home/homepage.dart';
 import 'package:assistantmemo/login/login.dart';
 import 'package:assistantmemo/notes/notes.dart';
 import 'package:assistantmemo/record/record.dart';
 import 'package:assistantmemo/profile/profile.dart';
+import 'package:flutter/material.dart';
 
 var appRoutes = {
   '/': (context) => const HomeScreen(),
   '/login': (context) => const LoginScreen(),
-  '/Record': (context) => const RecordScreen(),
+  '/Record': (context) => RecordScreen(onStop: (String path) {}),
+  // '/Record': (context) => const AudioRecorder(),
   '/Notes': (context) => const NotesScreen(),
   '/Profile': (context) => const ProfileScreen(),
 };
