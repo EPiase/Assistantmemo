8f2726cac879ddbc9165e7aa085fe8108d0a8435
alexciccolella125 <alexciccolella125@gmail.com>
2021-12-09 01:13:51 -0500

    git test changes

--- Flutter/lib/services/models.g.dart
+++ Flutter/lib/services/models.g.dart
@@ -7,14 +7,16 @@ part of 'models.dart';
 // **************************************************************************
 
 Note _$NoteFromJson(Map<String, dynamic> json) => Note(
+      json['note_id'] as String,
       DateTime.parse(json['date_recorded'] as String),
       audio_filename: json['audio_filename'] as String? ?? '',
-      classification: json['classification'] as String? ?? '',
+      classification: json['classification'] as String? ?? 'Short Note',
       is_starred: json['is_starred'] as bool? ?? false,
       text_transcript: json['text_transcript'] as String? ?? '',
     );
 
 Map<String, dynamic> _$NoteToJson(Note instance) => <String, dynamic>{
+      'note_id': instance.note_id,
       'audio_filename': instance.audio_filename,
       'classification': instance.classification,
       'date_recorded': instance.date_recorded.toIso8601String(),
